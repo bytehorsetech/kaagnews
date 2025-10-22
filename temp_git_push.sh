18b7e27c8ba7c261a813967201b4f55fa649dfbd
bytehorsetech <bytehorsetech@gmail.com>
2025-10-22 23:07:06 +0000

    Initial commit of KaagNews project

---
+++ temp_git_push.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git remote set-url origin https://github.com/bytehorsetech/kaagnews.git
+git add .
+git commit -m "Initial commit of KaagNews project"
+git push --set-upstream origin main
